813b726a3abcebec03c0da9985a8b8f46e374a3d
Michal2197 <slomimi2197_aehit@students.vizja.pl>
2026-05-22 13:27:13 +0200

    Dodanie szkieletu projektu MVC

---
+++ Projekt/SystemMonitorowaniaWydatkowDomowych/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SystemMonitorowaniaWydatkowDomowych.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
